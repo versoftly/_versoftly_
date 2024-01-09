95b03bd6326ef7ec06139c7231688f02a3285cdb
ramiro g. glez <mostlyphpsoftware@gmail.com>
2024-01-09 12:52:57 -0700

    hello world en java se

---
+++ versoftly_java_cpp/hello_world/src/main/java/versoftly/Hello_world.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package versoftly;
+
+/**
+ *
+ * @author versoftly
+ */
+public class Hello_world {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
