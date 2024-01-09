28516fa1a0d9cbab2f48b0553adebfc62c724280
ramiro g. glez <mostlyphpsoftware@gmail.com>
2024-01-09 13:19:10 -0700

    hello world in c language

---
+++ versoftly_java_cpp/hello_world/src/hello_world.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : hello_world.c
+ Author      : Ramiro g. glez.
+ Version     :
+ Copyright   : 2024
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main() {
+	printf("Hola Mundo!\n");
+	return 0;
+}
