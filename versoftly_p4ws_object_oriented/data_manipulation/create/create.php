7ebb0a1d6a9ad9a0c81a3fd7728b0618e56af6d2
ramiro g. glez <mostlyphpsoftware@gmail.com>
2024-01-12 09:01:48 -0700

    gc create implemented

--- versoftly_p4ws_object_oriented/data_manipulation/create/create.php
+++ versoftly_p4ws_object_oriented/data_manipulation/create/create.php
@@ -15,17 +15,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     } else {
         
-        $stmt = $conn->prepare("INSERT INTO registro (titulo,contenido,imagen,usuario) VALUES 
-        (:titulo,:contenido,:imagen,:usuario)");
-        
-        $stmt->execute([
-            ":titulo" => $titulo,
-            ":contenido" => $texto,
-            ":imagen" => "logo.jpg",
-            ":usuario" => $_SESSION['usuario']
-        ]);
-        
-        header("Location: path/file||url");
+        $gc = new GeneriCrud("insert");
+
+        $gc->executeThisOperation (
+            "registro",
+            [
+                "titulo",
+                "contenido",
+                "imagen",
+                "usuario"
+            ],
+            [
+                $titulo,
+                $texto,
+                "logo.jpg",
+                $_SESSION['usuario']
+            ],
+            "path|url"
+        );
+
     }
 
 }
