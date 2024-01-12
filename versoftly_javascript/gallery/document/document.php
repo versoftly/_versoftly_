ba37d90e5c257f2b6deac523febf4c5f4c5c427e
ramiro g. glez <mostlyphpsoftware@gmail.com>
2024-01-12 09:44:22 -0700

    starting the versoftly gallery, adding the starting document html structure to the index.php gallery file

---
+++ versoftly_javascript/gallery/document/document.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/Base.css">
+        <link rel="stylesheet" href="css/Normalize.css">
+        <link href="https://fonts.googleapis.com/css2?family=Krub:wght@400;700&display=swap"
+         rel="stylesheet">
+        <title>versoftly</title>
+    </head>
+    <body>
+
+        <?php
+        
+        ?>
+
+    </body>
+</html>
